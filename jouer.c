dbe926eb1191fdc4692e8feea8290ce168488c23
Aether <theaethersky@gmail.com>
2023-03-22 14:18:43 +0100

    norm update

--- jouer.c
+++ jouer.c
@@ -1,24 +1,27 @@
-#include <stdio.h>
 #include "tableau.h"
+#include <stdio.h>
 
 int	jouer(char tableau[11][11], int player)
 {
-  int i,j;
-  int bool = 0;
-  do {
-    printf("Entrez une colonne\n");
-    scanf("%d", &i);
-    printf("Entrez une ligne\n");
-    scanf("%d", &j);
-    if (tableau[i][j] == '.') {
-      if (player == 1) 
-        tableau[i][j] = 'X';
-      else 
-        tableau[i][j] = 'O';
-      bool = 1;
-    }
-    
-  }
-  while (bool != 1);
-  return 0;
+	int	bool;
+	int	i;
+	int	j;
+
+	bool = 0;
+	while (bool != 1)
+	{
+		printf("Entrez une colonne\n");
+		scanf("%d", &i);
+		printf("Entrez une ligne\n");
+		scanf("%d", &j);
+		if (tableau[i][j] == '.')
+		{
+			if (player == 1)
+				tableau[i][j] = 'X';
+			else
+				tableau[i][j] = 'O';
+			bool = 1;
+		}
+	}
+	return (0);
 }
--- tableau.h
+++ tableau.h
@@ -1,6 +1,6 @@
 #ifndef TABLEAU_H
 # define TABLEAU_H
 
-extern char    tableau[11][11];
+extern char	tableau[11][11];
 
 #endif
